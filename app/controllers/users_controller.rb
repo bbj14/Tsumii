3806e22dc650dd377504d83c19cfcf18e7b47b33
kazuya.makida <kazuya1425m@gmail.com>
2020-01-25 09:26:12 +0000

    first toppage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
   def index
+    #@users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
   end
 
   def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
   end
 end
