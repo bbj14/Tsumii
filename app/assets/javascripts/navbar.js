c3c0be718266e7c4b3bc0815ba74229855f6d2a2
kazuya.makida <kazuya1425m@gmail.com>
2020-02-08 08:02:41 +0000

    logo, UI

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -6,11 +6,11 @@ $(document).on('turbolinks:load', function() {
     $(window).scroll(function() {
       if ($('.toppage-content').length){
         if ($(this).scrollTop() > 0) {
-          $('nav').addClass('to-green');
+          $('nav').addClass('to-brown');
           $('nav').removeClass('to-none');
         } else {
           $('nav').addClass('to-none');
-          $('nav').removeClass('to-green');
+          $('nav').removeClass('to-brown');
         }
       };
     });
--- app/controllers/work_statuses_controller.rb
+++ app/controllers/work_statuses_controller.rb
@@ -5,7 +5,8 @@ class WorkStatusesController < ApplicationController
     @work = Work.find(params[:work_id])
     @status = params[:status]
     current_user.create_status(@work, @status)
-    flash[:success] = "作品を#{@status}に追加しました"
+    
+    flash[:success] = "作品を#{status_to_text(@status)}に追加しました"
     redirect_back(fallback_location: root_url)
   end
   
@@ -13,7 +14,8 @@ class WorkStatusesController < ApplicationController
     @work = Work.find(params[:work_id])
     @status = params[:status]
     current_user.change_status(@work, @status)
-    flash[:success] = "作品を#{@status}に変更しました"
+    
+    flash[:success] = "作品を#{status_to_text(@status)}に変更しました"
     redirect_back(fallback_location: root_url)
   end
   
@@ -21,7 +23,18 @@ class WorkStatusesController < ApplicationController
     @work = Work.find(params[:work_id])
     @status = params[:status]
     current_user.delete_status(@work)
-    flash[:success] = "作品を#{@status}から削除しました"
+    
+    flash[:success] = "作品を#{status_to_text(@status)}から削除しました"
     redirect_back(fallback_location: root_url)
+  end
+  
+  private
+  
+  def status_to_text(status)
+    if status == "solved"
+      "解答済"
+    elsif status == "solve_later"
+      "あとで解く"
+    end
   end
 end
