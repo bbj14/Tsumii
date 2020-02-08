b5575020ab95d3789d528c9a43e3291b146f17fe
kazuya.makida <kazuya1425m@gmail.com>
2020-02-08 11:13:11 +0000

    fix env

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,8 +7,8 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws(:access_key_id),
-      aws_secret_access_key: Rails.application.credentials.aws(:secret_access_key),
+      aws_access_key_id: ENV["AWS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],
       region: 'ap-northeast-1'  # S3バケット作成時に指定したリージョン。左記は東京を指す
     }
     config.fog_directory  = 'tsumii-image-store' # 作成したS3バケット名
