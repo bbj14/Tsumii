267dc3e627ea3fcfa4131e43d461a7378c225c4c
kazuya.makida <kazuya1425m@gmail.com>
2020-02-08 11:17:48 +0000

    fix assets
    change jquery file
    
    fix
    
    fix
    
    fix
    
    fix

--- app/assets/javascripts/ajax_response.js
+++ app/assets/javascripts/ajax_response.js
@@ -1,11 +1,11 @@
 $(document).on('turbolinks:load', function() {
   
-  $('[id^="follow-btn"]').on("ajax:success", (e) => {
-    $(e.currentTarget).html(e.detail[2].response);
+  $('[id^="follow-btn"]').on("ajax:success", function(event, data, status, xhr) {
+    $(event.currentTarget).html(data);
   });
   
-  $('[class^="like-btn"]').on("ajax:success", (e) => {
-    $("." + e.currentTarget.className).html(e.detail[2].response);
+  $('[class^="like-btn"]').on("ajax:success", function(event, data, status, xhr) {
+    $("." + event.currentTarget.className).html(data);
   });
   
 });
