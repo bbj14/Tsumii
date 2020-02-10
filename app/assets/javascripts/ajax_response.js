b9afa5aba82a1ea89d323b76c3191381b3e2cf40
kazuya.makida <kazuya1425m@gmail.com>
2020-02-10 04:48:34 +0000

    follower count ajax

--- app/assets/javascripts/ajax_response.js
+++ app/assets/javascripts/ajax_response.js
@@ -1,6 +1,21 @@
 $(document).on('turbolinks:load', function() {
   
   $('[id^="follow-btn"]').on("ajax:success", function(event, data, status, xhr) {
+    let state = $(event.currentTarget).find('.btn').val();
+    
+    if ($(event.currentTarget).parents('.modal').length == 0) {
+      if (state=="フォロー中") {
+        let count = $('.follower-count').text();
+        count--;
+        $('.follower-count').text(count);
+      }
+      if (state=="フォローする") {
+        let count = $('.follower-count').text();
+        count++;
+        $('.follower-count').text(count);
+      }
+    }
+    
     $(event.currentTarget).html(data);
   });
   
