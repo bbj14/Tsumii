07b5b461d916ca015c16e6d8c95b13bf266691d5
bbj14 <kazuya1425m@gmail.com>
2020-02-22 13:46:25 +0900

    circleci test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,12 +14,17 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+require 'webdrivers/chromedriver'
 require 'capybara/rspec'
 
 RSpec.configure do |config|
   
   config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
+    #driven_by :selenium_chrome_headless
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {
+      "args" => %w(--headless --disable-gpu --no-sandbox)
+    })
+    driven_by :selenium, using: :chrome, options: { desired_capabilities: caps }
   end
   
   # rspec-expectations config goes here. You can use an alternate
